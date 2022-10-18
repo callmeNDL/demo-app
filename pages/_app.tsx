4f9de8c47c1a2209813cebfaab213a8d3340c8c5
Nguyen Duc Long <longnd@MECORP.LOCAL>
2022-10-18 16:39:01 +0700

    update format scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
 import '../styles/globals.scss';
 import '../styles/icons.scss';
-
-import '../assets/scss/Header.scss';
-import '../assets/scss/Sider.scss';
-import '../assets/scss/CreateUser.scss';
-
-import '../assets/scss/pages/PageCreateUser.scss';
-import '../assets/scss/form/FormCreateUser.scss';
-import '../assets/scss/notification/Notification.scss';
-
+import '../assets/scss/main.scss';
 import 'antd/dist/antd.css';
 
 import type { AppProps } from 'next/app';
